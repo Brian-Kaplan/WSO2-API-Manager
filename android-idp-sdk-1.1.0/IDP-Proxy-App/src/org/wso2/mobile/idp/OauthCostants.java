4df4f1cda9d04a86d40e176fc6e048ec8c879158
Brian Kaplan <bakaplan2@gmail.com>
2016-04-13 14:22:03 -0400

    initial commit

---
+++ android-idp-sdk-1.1.0/IDP-Proxy-App/src/org/wso2/mobile/idp/OauthCostants.java
@@ -0,0 +1,8 @@
+package org.wso2.mobile.idp;
+
+public class OauthCostants {
+	final static String CLIENT_ID = "3fqVJirkcSru27ssOOfqPUQwtB4a";
+	final static String CLIENT_SECRET = "W6vMFPJ9j5iTYvJnoQAwcbSno4ga";
+	final static String CALL_BACK_URL = "http://wso2.com";
+	final static String INFO = "INFO";
+}
